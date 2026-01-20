dbc744feae3a7c0d8949d842df5eaa669f8b6e58
Barbieloth <kazikmaksim1@gmail.com>
2026-01-20 08:23:32 +0200

    Initial commit

---
+++ src/net/barbieloth/wordle/Main.java
@@ -0,0 +1,11 @@
+package net.barbieloth.wordle;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+    }
+}
