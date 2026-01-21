783d5a05c158b58d0164c87103f296c9277424fb
bebebeUserFKTSI <igoresha16082008@gmail.com>
2026-01-21 14:11:21 +0200

    1
    Added main menu and settings functionality for the game.

--- src/net/barbieloth/wordle/Main.java
+++ src/net/barbieloth/wordle/Main.java
@@ -3,34 +3,54 @@ package net.barbieloth.wordle;
 import java.util.*;
 
 public class Main {
+    private static int wordLenght = 5;
+    private static int maxAttempts = 6;
+
     public static void main(String[] args) {
-        Random rand = new Random();
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int attempts = 6;
-        List<String> words = Arrays.asList(
-                "арбуз", "банан", "ветер", "город", "дверь",
-                "зебра", "книга", "лодка", "метро", "носок",
-                "пирог", "радио", "спорт", "туман", "экран"
-        );
-        List<String> history = new ArrayList<>();
+        while (true){
+            showMainMenu();
+            String choise = sc.nextLine();
 
-        String main_word = words.get(rand.nextInt(words.size()));
+            switch (choise){
+                case "1": openSetting(sc); break;
+                case "2": openSetting(sc); break;
+                case "3": System.exit(0); break;
+                default: System.out.println("Невірний вибір!");
+            }
+        }
 
-        displayGrid(history, attempts);
 
     }
-    private static void displayGrid(List<String> history, int max) {
-        System.out.println("====== WORDLE ======");
 
-        for (String row : history) {
-            System.out.println(row);
-        }
-        for(int i = history.size(); i < max; i++) {
-            System.out.println("\u001B[48;5;240m   \u001B[0m ".repeat(5));
-        }
-        System.out.println("====================\n");
+    private static void showMainMenu(){
+        System.out.println("\n--- WORLDE --- ");
+        System.out.println("1. Старт");
+        System.out.println("2. Налаштування");
+        System.out.println("3. Вихід");
+        System.out.println("Оберіть опцію: ");
     }
+
+    private static void openSetting(Scanner sc){
+        while(true){
+            System.out.println("\n--- Налаштування --- ");
+            System.out.println("1. Довжина слова (зараз: " + wordLenght + ")");
+            System.out.println("2. Макс.спроб (зараз: " + maxAttempts + ")");
+            System.out.println("3. Вихід в головне меню");
+            String choise = sc.nextLine();
+            if (choise.equals("1")){
+                System.out.println("Введіть нову довжину: ");
+                wordLenght = Integer.parseInt(sc.nextLine());
+            } else if (choise.equals("2")){
+                System.out.println("Введіть нову кількість спроб: ");
+                maxAttempts = Integer.parseInt(sc.nextLine());
+            } else if (choise.equals("3")) break;
+        }
+    }
+
+
+
 
 
 }
